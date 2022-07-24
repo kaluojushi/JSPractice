fa246bb8587fa992c627d8a764622842932f50e8
kaluojushi <694802801@qq.com>
2022-07-24 17:03:09 +0800

    2022-07-24 modifying 2022-07-daily.js

--- leetcode/2022-07-daily.js
+++ leetcode/2022-07-daily.js
@@ -6,9 +6,24 @@
  */
 
 /**
- * @param {number[]} arr
+ * @param {number[]} distance
+ * @param {number} start
+ * @param {number} destination
  * @return {number}
  */
-var lenLongestFibSubseq = function(arr) {
-
+var distanceBetweenBusStops = function(distance, start, destination) {
+  const n = distance.length;
+  const d = [0, 0];
+  let idx = 0;
+  for (let i = start, cnt = 0; cnt < n; i = (i + 1) % n, cnt++) {
+    if (i === destination) {
+      idx = 1;
+    }
+    d[idx] += distance[i];
+  }
+  return Math.min(...d);
 };
+
+console.log(distanceBetweenBusStops([1,2,3,4],0,1));
+console.log(distanceBetweenBusStops([1,2,3,4],0,2));
+console.log(distanceBetweenBusStops([1,2,3,4],0,3));
