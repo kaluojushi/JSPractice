99ddf58dac843b59092f31c25898f26ba71fc6f9
kaluojushi <694802801@qq.com>
2022-08-14 15:46:57 +0800

    2022-08-14 modifying 2022-08-daily.js

---
+++ README.md
@@ -0,0 +1,11 @@
+# JSPractice
+一些JS练习（GitHub同步）
+
+部分文件与文件夹说明：
+
+- `algorithm`：算法
+- `leetcode`：力扣刷题
+- `structure`：数据结构
+- `venue`：场馆预约练习
+- `PriorityQueue.js`：JS优先队列模板（基于堆）
+- `Trie.js`：JS前缀树模板（基于object）
---
+++ leetcode/2022-08-daily.js
@@ -0,0 +1,56 @@
+/**
+ * @author MZJ
+ * @date 2022-08
+ * @description 每日刷题
+ * @copyright LeetCode
+ */
+
+/** 在这里定义一些预定义的类，如TreeNode等 */
+
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var maxChunksToSorted = function(arr) {
+  const n = arr.length;
+  const sortedArr = [...arr].sort((a, b) => a - b);
+  const cnt = new Map();
+  let ans = 0;
+  for (let i = 0; i < n; i++) {
+    const a = arr[i], b = sortedArr[i];
+    cnt.set(a, (cnt.get(a) || 0) + 1);
+    if (cnt.get(a) === 0) {
+      cnt.delete(a);
+    }
+    cnt.set(b, (cnt.get(b) || 0) - 1);
+    if (cnt.get(b) === 0) {
+      cnt.delete(b);
+    }
+    if (cnt.size === 0) {
+      ans++;
+    }
+  }
+  return ans;
+};
+
+// console.log(maxChunksToSorted([5,4,3,2,1]));
+// console.log(maxChunksToSorted([2,1,3,4,4]));
+
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var maxScore = function(s) {
+  let max = 0;
+  let score = [...s].filter(ch => ch === '1').length;
+  for (let i = 0; i < s.length - 1; i++) {
+    score += s[i] === '0' ? 1 : -1;
+    max = Math.max(max, score);
+  }
+  return max;
+};
+
+console.log(maxScore("011101"));
+console.log(maxScore("00111"));
+console.log(maxScore("1111"));
